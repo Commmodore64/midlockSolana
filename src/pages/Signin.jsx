1fe3be0b36312520cc3d9ffaf574d7c3c5b710b4
Commmodore64 <cesarml2002@hotmail.com>
2023-06-01 10:52:52 -0700

    Fondo de signin

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Signin() {
+  return (
+    <div className="Signin w-screen h-screen">
+      <div className="circle1"></div>
+      <div className="circle2"></div>
+      <h1 className=" text-3xl font-semibold text-black top-20 ml-5">Login Account</h1> 
+      <h2 className="absolute text-xl font-semibold text-black top-28 ml-5">Welcome back !</h2> 
+    </div>
+  );
+}
+
+export default Signin;
