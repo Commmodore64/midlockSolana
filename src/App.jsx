aab84811cae7295b9da5b6faf03d3e69f2ef108b
Commmodore64 <cesarml2002@hotmail.com>
2023-06-06 15:04:14 -0700

    login aun por terminar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Signin from "./pages/Signin";
 import Signup from './pages/Signup';
--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
+import Logo from "../assets/LogoSinFondo.png";
+import Wallpaper5 from "../assets/wallpaper5.svg";
 
 function Signin() {
   return (
-    <div className="Signin w-screen h-screen">
-      <div className="circle1"></div>
-      <div className="circle2"></div>
-      <h1 className=" text-3xl font-semibold text-black top-20 ml-5">Login Account</h1> 
-      <h2 className="absolute text-xl font-semibold text-black top-28 ml-5">Welcome back !</h2> 
-    </div>
+    <div
+      className="w-screen h-screen absolute inset-0 z-[-1]"
+      style={{
+        backgroundImage: `url(${Wallpaper5})`,
+        backgroundRepeat: "repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <h1 className=" text-3xl font-semibold text-black top-12 ml-5">
+        Login Account
+      </h1>
+      <h2 className="absolute text-xl font-semibold text-black top-20 ml-5">
+        Welcome back !
+      </h2>
+      <div className="relative z-10 top-28 flex items-center justify-center">
+        <img src={Logo} alt="LogoSinFondo" className="h-40 object-contain" />
+        <h1 className="flex text-black">Medi</h1>
+        <h1 className="text-[#9AC1F0]">Lock</h1>
+      </div>
+      </div>
   );
 }
 
