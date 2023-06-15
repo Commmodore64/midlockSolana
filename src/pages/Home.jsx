f8cf8f344530f8adb0cbe46fe5bc8036cfddcebd
Commmodore64 <cesarml2002@hotmail.com>
2023-06-15 07:23:18 -0700

    Imprimiendo datos en UI de la base de datos

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -32,12 +32,12 @@ const Home = () => {
       fetchData();
     } else {
       console.log("No hay usuario autenticado");
-      window.location.href = "/home";
+      window.location.href = "/";
     }
   }, []);
-  const storedUIDString = storedUID.toString();
+  //const storedUIDString = storedUID.toString();
 
-  console.log(data[0]);
+  //console.log(data[0]);
   return (
     <div
       className="w-screen h-screen flex"
@@ -51,19 +51,19 @@ const Home = () => {
         Home
         <h2 className="text-xl font-semibold mt-10 ml-1">Med list</h2>
       </div>
-      <div className="flex flex-col mt-4">
-    {data.map((item) => {
-      const key = Object.keys(item)[0]; // Obtiene la clave dinámica
-      const { med, des } = item[key]; // Obtiene las propiedades med y des
-
-      return (
-        <div key={key} className="bg-white rounded-xl shadow p-4 mb-4">
-          <p>{med}</p>
-          <p>{des}</p>
-        </div>
-      );
-    })}
-  </div>
+      <div className="flex flex-col mt-36">
+        {data.map((item) => {
+          const keys = Object.keys(item); // Obtiene las claves del objeto
+          return keys
+            .filter((key) => key !== "id") // Filtra las claves y elimina la clave 'id'
+            .map((key) => (
+              <div key={key} className="bg-white rounded-xl shadow p-4 mb-4">
+                <p>{item[key].med}</p>
+                <p>{item[key].des}</p>
+              </div>
+            ));
+        })}
+      </div>
       <TabBar />
     </div>
   );
