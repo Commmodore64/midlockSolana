f8e188ab01af39c28e33964307799cbf80e65350
Commmodore64 <cesarml2002@hotmail.com>
2023-07-10 07:26:46 -0700

    Edit agregado listado de medicamentos, imagenes y informacion

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Wallpaper from "../assets/wallpaper.svg";
 import TabBar from "../components/TabBar";
-import { storage }  from "../firebase/firebase.config";
-import {getStorage, ref, getDownloadURL} from "firebase/storage";
-import react, { useEffect, useState } from 'react';
+import { storage, firestore } from "../firebase/firebase.config";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { getFirestore, collection, addDoc, getDocs } from "firebase/firestore";
 
 function Edit() {
-  const [imageUrl, setImageUrl] = useState('');
+  const [medList, setMedList] = useState([]);
+
   useEffect(() => {
-    const getImageUrl = async () => {
-      try{
-        const storage = getStorage();
-        const url = await getDownloadURL(ref(storage, "images/Parecetamol750mg.png"));
-        // Actualiza el estado con la URL de la imagen
-        setImageUrl(url);
-      } catch(error) { 
-        console.log('Error al obtener la URL de la imagen: ',error);
+    const fetchData = async () => {
+      try {
+        const firestoreInstance = getFirestore();
+        const medListRef = collection(firestoreInstance, "medlist");
+        const querySnapshot = await getDocs(medListRef);
+        const medListData = [];
+
+        for (const doc of querySnapshot.docs) {
+          const { medicamento, descripcion, imageName } = doc.data();
+          const storageInstance = getStorage();
+          const imageUrl = await getDownloadURL(
+            ref(storageInstance, `images/${imageName}`)
+          );
+
+          medListData.push({
+            id: doc.id,
+            medicamento,
+            descripcion,
+            imageUrl,
+          });
+        }
+
+        setMedList(medListData);
+        console.log(medListData);
+      } catch (error) {
+        console.log("Error al obtener la lista de medicamentos: ", error);
       }
     };
-    getImageUrl();
+
+    fetchData();
   }, []);
+
   return (
     <div
       className="w-screen h-screen flex flex-col"
@@ -29,14 +50,29 @@ function Edit() {
         backgroundSize: "cover",
       }}
     >
-      <div className="flex flex-col font-semibold text-4xl mt-12 ml-4">
-        Edit
-        <h2 className="flex items-center justify-between text-xl font-semibold mt-10 ml-4">
-          <span className="mr-2">Med list</span>
-        </h2>
-        <div>
-          {imageUrl && <img src={imageUrl} alt="Image" />}
-        </div>
+      <div className="flex font-semibold text-4xl mt-12 ml-4">Edit</div>
+      <h2 className="flex items-center justify-between text-xl font-semibold mt-10 ml-4">
+        <span className="mr-2">Med list</span>
+      </h2>
+      <div className="flex flex-wrap justify-center mt-4">
+        {medList.map((med) => (
+                                 <button>
+            <div
+              key={med.id}
+              className="bg-white rounded-2xl shadow-lg p-5 mx-5 mb-5 w-52 h-60"
+            >
+              <img
+                className="object-contain w-32 h-38"
+                src={med.imageUrl}
+                alt="Image"
+              />
+              <div className="text-center">
+                <h2 className="text-xl font-semibold">{med.medicamento}</h2>
+                <span className="text-sm">{med.descripcion}</span>
+              </div>
+            </div>
+          </button>
+        ))}
       </div>
       <TabBar />
     </div>
